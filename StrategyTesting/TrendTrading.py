6829df27d396a19946d20aa0be23669a8152b7e0
cadofa <cadofa@163.com>
2024-03-29 17:48:47 +0800

    New idea

--- StrategyTesting/TrendTrading.py
+++ StrategyTesting/TrendTrading.py
@@ -15,31 +15,84 @@ def save_close_price(close):
 
 TEST_COUNT = 2
 
+
 tick_data_list = []
-dev_value_list = []
+tick_mean = 0
+
+trend_config = [
+    {
+        "obser_period": 21,
+        "mean_list": [],
+        "tick_list": [],
+        "position": [],
+        "peak_value": 0,
+        "close_gain": 0,
+    },
+]
 
 def onTick(tick):
-    global tick_data_list, dev_value_list
+    global trend_config, tick_data_list, tick_mean
     #print
     #print "行情数据-------------------*----------------------",  tick
     tick_data_list.append(tick)
-    #print tick_data_list
     tick_mean = np.mean(tick_data_list)
-    dev_value_list.append((tick - tick_mean)/tick_mean*100)           
-    #time.sleep(1)
+    for t in trend_config:
+        #print "持仓数据", t["position"]
+        #print "持仓参照峰值", t["peak_value"]
+        t["tick_list"].append(tick)
+        if len(t["tick_list"]) > t["obser_period"]:
+            t["tick_list"].pop(0)
+        t["mean_list"].append(tick_mean)
+        if len(t["mean_list"]) > t["obser_period"]:
+            t["mean_list"].pop(0)
+        #print t["tick_list"]
+        #print t["mean_list"]
+        
+        if (t['tick_list'][0] > t['mean_list'][0] 
+            and t["tick_list"][1] < t["mean_list"][1] and tick < tick_mean):
+            if not t["position"]:
+                #print "下穿均线做空"
+                #print "空单开仓", tick - 1, "S"
+                t["position"].append((tick - 1, "S"))
+                t["peak_value"] = tick - 1
+        if (t['tick_list'][0] < t['mean_list'][0] 
+            and t["tick_list"][1] > t["mean_list"][1] and tick > tick_mean):
+            if not t["position"]:
+                #print "上穿均线做多"
+                #print "多单开仓", tick + 1, "B"
+                t["position"].append((tick + 1, "B"))
+                t["peak_value"] = tick + 1
+        if t["position"]:
+            if t["position"][0][1] == "B" and tick > t["peak_value"]:
+                t["peak_value"] = tick
+            if t["position"][0][1] == "S" and tick < t["peak_value"]:
+                t["peak_value"] = tick
+            if t["position"][0][1] == "B" and (t["peak_value"] - tick) >= t["obser_period"]:
+                #print "多单平仓", tick - 1
+                t["close_gain"] += (tick - 1) - t["position"][0][0]
+                t["peak_value"] = 0
+                t["position"].remove(t["position"][-1])
+                continue
+            if t["position"][0][1] == "S" and (tick - t["peak_value"]) >= t["obser_period"]:
+                #print "空单平仓", tick + 1
+                t["close_gain"] += t["position"][0][0] - (tick + 1)
+                t["peak_value"] = 0
+                t["position"].remove(t["position"][-1])
+                continue
+
+        
+    #time.sleep(2)
 
 def test_strategy():
     market_data = create_index_data()
     for i in market_data:
         onTick(i)
-
-    print "均值", np.mean(tick_data_list)
-    print "最大最小偏离", max(dev_value_list), min(dev_value_list)
         
 
 for i in range(1, TEST_COUNT):
     fut_data = test_strategy()
     #time.sleep(2)
+    print trend_config 
          
 #    ypoints = np.array(fut_data)
 #    plt.plot(ypoints)
