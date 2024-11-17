d8da4ad62c3d5622c62906ccaefa6ff30430b0e5
caidongfang <cadofa@163.com>
2024-11-17 22:58:08 +0800

    new strategy

--- StrategyTesting/HedgingTrans.py
+++ StrategyTesting/HedgingTrans.py
@@ -19,8 +19,10 @@ def save_close_price(close):
     with open('close.pk', 'wb') as file:
         pickle.dump(close, file)
 
-SAMPLE_SIZE = 3600
-TEST_COUNT = 4 * 5 + 1
+SAMPLE_SIZE = 3600 
+TEST_COUNT = 4 + 1
+
+B_S_DIFF = 2
 
 step_interval = [5,6,8,10,13,15,18,21,34,55,34,21,18,15,13,10]
 
@@ -73,61 +75,68 @@ def test_strategy():
     for i in market_data:
         print
         print "多单持仓详情", B_Position_list
+        print "空单持仓详情", S_position_list
         print "----------------------------最新行情---------------------------------------", i,"------------"*2 , x_c
         print
         x_c += 1
         #初始化建仓
-        if not B_Position_list:
+        if not B_Position_list and (len(B_Position_list) - len(S_position_list) <= B_S_DIFF):
             B_Position_list.append(i + 1)
             print "多单初始化建仓"
             print "买入开仓", i+1
             print "多单持仓详情", B_Position_list
+            print "空单持仓详情", S_position_list
             B_operation_stack.append((x_c, i + 1, "B"))
 
         #初始化建仓
-        if not S_position_list:
+        if not S_position_list and (len(S_position_list) - len(B_Position_list) <= B_S_DIFF):
             S_position_list.append(i - 1)
             print "空单初始化建仓"
             print "卖出开仓", i+1
             print "空单持仓详情", S_position_list
+            print "多单持仓详情", B_Position_list
             S_operation_stack.append((x_c, i - 1, "S"))
 
         #有过操作，没有持仓
-        if B_operation_stack and not B_Position_list:
+        if B_operation_stack and not B_Position_list and (len(B_Position_list) - len(S_position_list) <= B_S_DIFF):
             #如果上一次是卖出，当前价格比上一次卖出价格低出步长，继续买
             if B_operation_stack[-1][2] == "S" and B_operation_stack[-1][1] - i >= touch_step:
                 B_Position_list.append(i + 1)
                 print "上一次操作是卖出，当前价格比上一次卖出价格低出摸顶步长，继续买"
                 print "多单买入开仓", i+1
                 print "多单持仓详情", B_Position_list
+                print "空单持仓详情", S_position_list
                 B_operation_stack.append((x_c, i + 1, "B"))
 
         #有过操作，没有持仓
-        if S_operation_stack and not S_position_list:
+        if S_operation_stack and not S_position_list and (len(S_position_list) - len(B_Position_list) <= B_S_DIFF):
             #如果上一次是买入，当前价格比上一次买入价格高出步长，继续卖
             if S_operation_stack[-1][2] == "B" and i - S_operation_stack[-1][1] >= touch_step:
                 S_position_list.append(i - 1)
                 print "上一次操作是买入，当前价格比上一次买入价格高出摸底步长，继续卖"
                 print "空单卖出开仓", i - 1
                 print "空单持仓详情", S_position_list
+                print "多单持仓详情", B_Position_list
                 S_operation_stack.append((x_c, i - 1, "S"))
         
         B_last_index = B_Position_list.index(B_Position_list[-1])
         #根据买入步长逐步买进
-        if (B_Position_list[-1] - i) >= step_interval[B_last_index]:
+        if (B_Position_list[-1] - i) >= step_interval[B_last_index] and (len(B_Position_list) - len(S_position_list) <= B_S_DIFF):
             B_Position_list.append(i + 1)
             print "当前点位比最后持仓点位低出指定间隔步长继续买入开仓"
             print "多单买入开仓", i+1
             print "多单持仓详情", B_Position_list
+            print "空单持仓详情", S_position_list
             B_operation_stack.append((x_c, i + 1, "B"))
         
         S_last_index = S_position_list.index(S_position_list[-1])
         #根据卖出步长逐步卖出
-        if (i - S_position_list[-1]) >= step_interval[S_last_index]:
+        if (i - S_position_list[-1]) >= step_interval[S_last_index] and (len(S_position_list) - len(B_Position_list) <= B_S_DIFF):
             S_position_list.append(i - 1)
             print "当前点位比最后持仓点位高出指定间隔步长继续卖出开仓"
             print "空单卖出开仓", i - 1
             print "空单持仓详情", S_position_list
+            print "多单持仓详情", B_Position_list
             S_operation_stack.append((x_c, i - 1, "S"))
 
         #如果比上一次买入高指定步长，卖出
@@ -138,6 +147,7 @@ def test_strategy():
             B_profit_loss_this_close = (i - 1)  - B_Position_list[-1]
             B_Position_list.remove(B_Position_list[-1])
             print "多单持仓详情", B_Position_list
+            print "空单持仓详情", S_position_list
             B_profit_loss["B_profit_loss_close"] = B_profit_loss["B_profit_loss_close"] + B_profit_loss_this_close
             print "多单平仓收益", B_profit_loss
 
@@ -149,6 +159,7 @@ def test_strategy():
             S_profit_loss_this_close = S_position_list[-1] - (i + 1)
             S_position_list.remove(S_position_list[-1])
             print "空单持仓详情", S_position_list
+            print "多单持仓详情", B_Position_list
             S_profit_loss["S_profit_loss_close"] = S_profit_loss["S_profit_loss_close"] + S_profit_loss_this_close
             print "空单平仓收益", S_profit_loss
 
@@ -160,6 +171,7 @@ def test_strategy():
             B_profit_loss_this_close = i - 1 - B_Position_list[-1]
             B_Position_list.remove(B_Position_list[-1])
             print "多单持仓详情", B_Position_list
+            print "空单持仓详情", S_position_list
             B_profit_loss["B_profit_loss_close"] = B_profit_loss["B_profit_loss_close"] + B_profit_loss_this_close
             print "多单平仓收益", B_profit_loss
 
@@ -171,6 +183,7 @@ def test_strategy():
             S_profit_loss_this_close = S_position_list[-1] - (i + 1)
             S_position_list.remove(S_position_list[-1])
             print "空单持仓详情", S_position_list
+            print "多单持仓详情", B_Position_list
             S_profit_loss["S_profit_loss_close"] = S_profit_loss["S_profit_loss_close"] + S_profit_loss_this_close
             print "空单平仓收益", S_profit_loss
 
