314734380ea81dd864afeaba0d7a1117a28c86a8
cadofa <cadofa@163.com>
2025-11-23 01:16:57 +0800

    添加天勤量化

---
+++ StrategyTesting/Tianqin/tqsdk_copy_top.py
@@ -0,0 +1,90 @@
+import logging
+from datetime import date
+from tqsdk import TqApi, TqAuth, TqSim, TqBacktest
+import time, math
+
+# 创建API连接
+api =  TqApi(account=TqSim(init_balance=100000),
+             backtest=TqBacktest(start_dt=date(2025, 9, 20), end_dt=date(2025, 11, 21)),
+             web_gui=True, 
+             auth=TqAuth("cadofa", "cadofa6688"))
+#symbol = "DCE.m2601"  # 修改为你需要的合约
+symbol = "CZCE.FG601"
+#symbol = "CZCE.SA601"
+
+# 获取行情数据
+quote = api.get_quote(symbol)
+klines = api.get_kline_serial(symbol, 60, data_length=100)  # 1分钟K线，保留100根
+short_position_list = []
+copy_top_step = [5,6,8,10,13,15,18,21,34,55,89,55,34,21,18,15,13,10]
+
+def open_short_position():
+    order = api.insert_order(symbol=symbol, direction="SELL", offset="OPEN", volume=1)
+    print(f"已提交空单订单")
+
+    # 等待订单成交
+    while order.status == "ALIVE":
+        api.wait_update()
+        print(f"订单状态: {order.status}")
+
+    # 检查最终状态
+    if order.status == "FINISHED":
+        print("✅ 空单建仓成功!")
+        if not math.isnan(order.trade_price):
+            short_position_list.append(order.trade_price)
+        position = api.get_position(symbol)
+        print(f"持仓: 空单{position.pos}手, 持仓列表{short_position_list}, 持仓均价: {position.open_price_short}")
+    else:
+        print(f"❌ 订单异常: {order.status}")
+
+def close_short():
+    order = api.insert_order(symbol=symbol, direction="BUY", offset="CLOSE", volume=1)
+    print("空单平仓订单已提交")
+    
+    # 等待订单成交
+    while order.status == "ALIVE":
+        api.wait_update()
+    
+    if order.status == "FINISHED":
+        print("✅ 空单平仓成功")
+        short_position_list.remove(short_position_list[-1])
+    else:
+        print(f"平仓失败，订单状态: {order.status}")
+
+logging.getLogger("tqsdk").setLevel(logging.WARNING)
+try:
+    while True:
+        api.wait_update()  # 等待数据更新
+        # 获取最新价格
+        if quote.datetime != 0:
+            latest_price = quote.last_price
+            #print(f"最新价格: {latest_price}", end=" | ")
+        
+        # 计算60周期均线
+        if len(klines) >= 60:
+            ma_60 = klines.close.iloc[-60:].mean()
+            #print(f"MA60: {ma_60:.2f}")
+        else:
+            print(f"K线数量: {len(klines)}/60，均线计算中...")
+        
+        position = api.get_position(symbol)
+        if quote.last_price < ma_60:
+            if position.volume_short == 0:
+                open_short_position()
+
+            if short_position_list:
+                last_index = short_position_list.index(short_position_list[-1])
+                if (quote.last_price - short_position_list[-1]) >= copy_top_step[last_index]:
+                    open_short_position()
+
+        if short_position_list:
+            dynamic_step = quote.last_price * 0.01
+            if (short_position_list[-1] - quote.last_price) >= dynamic_step:
+                close_short()
+            
+        #time.sleep(1)
+except KeyboardInterrupt:
+    print("\n程序结束")
+
+finally:
+    api.close()
